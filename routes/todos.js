30d8fc1c35f233974feee54690fb697aab2f6d18
muizu <muizuvais99@gmail.com>
2025-08-11 14:21:07 +0530

    added pool

--- routes/todos.js
+++ routes/todos.js
@@ -1,49 +1,69 @@
+import { Pool } from "pg";
 import express from "express";
 
 const router = express.Router();
 
-const todos = [];
+const pool = new Pool({
+  user: "me",
+  host: "localhost",
+  database: "api",
+  password: "password",
+  port: 5432,
+});
 
 router
   .route("/")
   .get((_req, res) => {
-    return res.json(todos);
+    pool.query("SELECT * FROM todos ORDER BY id ASC", (error, results) => {
+      if (error) {
+        return res.status(400).json({ error });
+      }
+
+      return res.status(200).json(results.rows);
+    });
   })
   .post((req, res) => {
-    const todo = {
-      id: todos.length + 1,
-      title: req.body.title,
-      completed: req.body.completed || false,
-    };
+    const { title, completed } = req.body;
 
-    todos.push(todo);
+    pool.query(
+      `INSERT INTO todos (title, completed) VALUES($1, $2) RETURNING *`,
+      [title, completed],
+      (error, results) => {
+        if (error) {
+          return res.status(400).json({ error });
+        }
 
-    res.status(201).json(todo);
+        return res
+          .status(201)
+          .send(`User added with ID: ${results.rows[0].id}`);
+      }
+    );
   });
 
 router
   .route("/:id")
   .put((req, res) => {
     const id = parseInt(req.params.id);
-    const todo = todos.find((t) => t.id === id);
-    if (!todo) {
-      return res.status(404).json({ error: "Error: todo not found!" });
-    }
-
-    todo.title = req.body.title || todo.title;
-    todo.completed = req.body.completed || todo.completed;
-
-    res.json(todo);
+    const { title, completed } = req.body;
+    pool.query(
+      "UPDATE todos SET title = $1, completed = $2 WHERE id = $3",
+      [title, completed, id],
+      (error, _results) => {
+        if (error) {
+          return res.status(400).json({ error });
+        }
+        return res.status(200).send(`User modified with ID: ${id}`);
+      }
+    );
   })
   .delete((req, res) => {
     const id = parseInt(req.params.id);
-    const index = todos.findIndex((t) => t.id === id);
-    if (index < 0) {
-      return res.status(404).json({ error: "Error: id not found!" });
-    }
-
-    todos.splice(index, 1);
-    res.status(204).send();
+    pool.query("DELETE FROM todos WHERE id = $1", [id], (error, _results) => {
+      if (error) {
+        return res.status(400);
+      }
+      return res.status(200).send(`User deleted with ID: ${id}`);
+    });
   });
 
 export default router;
