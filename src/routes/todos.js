3023679e8b058c458d7c16a5ef228130e8b90094
muizu <muizuvais99@gmail.com>
2025-08-12 17:43:41 +0530

    added next

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -30,11 +30,22 @@ router
 
 router
   .route("/:id")
+  .get(async (req, res, next) => {
+    const id = parseInt(req.params.id);
+    try {
+      const { rows } = await pool.query("SELECT * FROM todos WHERE id = $1", [
+        id,
+      ]);
+      res.json({ todo: rows[0] });
+    } catch (error) {
+      next(error);
+    }
+  })
   .put(validate(updateSchema), async (req, res, next) => {
     const id = parseInt(req.params.id);
     const { title, completed } = req.body;
     try {
-      const { rows } = await pool.query(
+      await pool.query(
         "UPDATE todos SET title = $1, completed = $2 WHERE id = $3",
         [title, completed, id]
       );
