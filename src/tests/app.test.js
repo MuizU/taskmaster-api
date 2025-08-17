0b533cc78b40ac94b62e971689343c27592d7da6
muizu <muizuvais99@gmail.com>
2025-08-17 16:55:24 +0530

    fixed issue

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,9 +1,10 @@
 import request from "supertest";
-import app from "../app.js";
 import { describe, expect } from "@jest/globals";
+import createApp from "../app.js";
 
 describe("GET /", () => {
   it("should return HELLO WORLD", async () => {
+    const app = createApp();
     const res = await request(app).get("/");
     expect(res.status).toBe(200);
     expect(res.text).toBe("HELLO WORLD");
