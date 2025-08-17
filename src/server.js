f9853733c1a633601c0a89f1c34777c8b1bf225b
muizu <muizuvais99@gmail.com>
2025-08-17 16:00:12 +0530

    added graceful shutdown

--- src/server.js
+++ src/server.js
@@ -1,8 +1,30 @@
 import "dotenv/config";
 import app from "./app.js";
+import { pool } from "./db.js";
 
 const PORT = process.env.PORT || 9000;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
+});
+
+const shutdown = async (signal) => {
+  try {
+    await new Promise((resolve, reject) => {
+      ServiceWorkerRegistration.close((error) =>
+        error ? reject(error) : resolve()
+      );
+    });
+
+    await pool.end(); // closing the posrgres pool
+    console.log("Database pool closed server stopped.");
+    process.exit(0);
+  } catch (error) {
+    console.log("error during shutdown");
+    process.exit(1);
+  }
+};
+
+["SIGINT", "SIGTERM"].forEach((signal) => {
+  process.on(signal, () => shutdown(signal));
 });
